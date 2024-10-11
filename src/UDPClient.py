11651a909ee5a17161aff9e6b9cebea324de4429
Bagas Noor <18223115@std.stei.itb.ac.id>
2024-10-11 22:24:46 +0700

    feat: Implementing a UDP Server as well as client side for testing purpose

---
+++ src/UDPClient.py
@@ -0,0 +1,15 @@
+from socket import *
+import random
+import string
+import time
+
+serverName = "192.168.6.57"
+serverPort = 50000
+clientSocket = socket(AF_INET,SOCK_DGRAM)
+message = input("Masukan nama anda:")
+clientSocket.sendto(message.encode(),(serverName,serverPort))
+while True:
+    pesan, serverAddress = clientSocket.recvfrom(2048)
+    message ="Permintaan acak"
+    clientSocket.sendto(message.encode(),(serverName,serverPort))
+    print("Bilangan Acak dari server("+str(serverAddress)+"): "+str(pesan.decode()))
--- src/socketUDP.py
+++ src/socketUDP.py
@@ -1,1 +1,17 @@
-import socket
+from socket import *
+import time
+import random
+import threading
+import queue
+
+serverPort = 50000
+serverIP ="192.168.6.57"
+serverSocket = socket(AF_INET, SOCK_DGRAM)
+serverSocket.bind((serverIP, serverPort))
+print("Server bilangan acak")
+while True:
+    message, clientAddress = serverSocket.recvfrom(2048)
+    print(str(message.decode())+" dari:"+str(clientAddress))
+    bilAcak = random.randint(6,20)
+    time.sleep(1)
+    serverSocket.sendto(str(bilAcak).encode(),clientAddress)
