d975face759fb8dee22cb18519abcd7904adec4b
Bagas Noor <18223115@std.stei.itb.ac.id>
2024-10-13 17:30:00 +0700

    feat: Added full functionality of UDP chatrooms

--- src/UDPClient.py
+++ src/UDPClient.py
@@ -1,15 +1,34 @@
 from socket import *
-import random
-import string
-import time
+import threading
 
 serverName = "192.168.6.57"
 serverPort = 50000
 clientSocket = socket(AF_INET,SOCK_DGRAM)
-message = input("Masukan nama anda:")
-clientSocket.sendto(message.encode(),(serverName,serverPort))
+
+def receive_message():
+    while True:
+        try:
+            message, _ = clientSocket.recvfrom(2048)
+            print("\n" + message.decode())
+        except:
+            break
+
+
 while True:
-    pesan, serverAddress = clientSocket.recvfrom(2048)
-    message ="Permintaan acak"
+    passkey = input("Masukkan Passkey: ")
+    clientSocket.sendto(passkey.encode(),(serverName,serverPort))
+    Response, _ =clientSocket.recvfrom(2048)
+    if Response.decode() == 'OK':
+        print("Passkey benar, kamu sudah masuk chatroom")
+        break
+    else:
+        print(Response.decode())
+
+username = input("Masukkan nama anda: ")
+clientSocket.sendto(username.encode(), (serverName,serverPort))
+
+threading.Thread(target=receive_message, daemon=True).start()
+
+while True:
+    message = input("")
     clientSocket.sendto(message.encode(),(serverName,serverPort))
-    print("Bilangan Acak dari server("+str(serverAddress)+"): "+str(pesan.decode()))
--- src/socketUDP.py
+++ src/socketUDP.py
@@ -1,17 +1,38 @@
 from socket import *
-import time
-import random
-import threading
-import queue
 
 serverPort = 50000
-serverIP ="***"
+serverIP ="192.168.6.57"
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind((serverIP, serverPort))
-print("Server bilangan acak")
+
+Clients = {}
+Passkey = "123"
+
+print("Server is running . . . .")
+
 while True:
-    message, clientAddress = serverSocket.recvfrom(2048)
-    print(str(message.decode())+" dari:"+str(clientAddress))
-    bilAcak = random.randint(6,20)
-    time.sleep(1)
-    serverSocket.sendto(str(bilAcak).encode(),clientAddress)
+    try:
+        message, clientAddress = serverSocket.recvfrom(2048)
+        decoded_message = message.decode()
+
+        if clientAddress not in Clients:
+            if (decoded_message == Passkey):
+                serverSocket.sendto("OK".encode(),(clientAddress))
+                username, clientAddress = serverSocket.recvfrom(2048)
+                Clients[clientAddress] = username.decode()
+                print(f"Client {clientAddress} just joined with {Passkey}")
+                print(f"Client {clientAddress} set their username as {Clients[clientAddress]}")
+            else:
+                serverSocket.sendto("Wrong Passkey".encode(),(clientAddress))
+                print(f"Client {clientAddress} gives wrong passkey access denied")
+        else:
+            username = Clients[clientAddress]
+            print(f"Message from {Clients[clientAddress]}: {decoded_message}")
+            for user in Clients:
+                if user != clientAddress:
+                    serverSocket.sendto(f"{username}: {decoded_message}".encode(), user)
+
+    except ConnectionError as e:
+        print(f"Connection reset by {clientAddress}: {e}")
+        continue
+
