5bf17005b53e08aecba70fb199da74f8e7cc7d28
Mark(ntrlnm) <166894547+DoItMark@users.noreply.github.com>
2024-10-19 11:14:15 +0700

    Update and rename src/ClientUDP.py to UDPClient.py
    change 
    file name
    copying code from Bagas/src/UDPClient

---
+++ UDPClient.py
@@ -0,0 +1,39 @@
+
+from socket import *
+import threading
+
+serverName = ""
+serverPort = 50000
+clientSocket = socket(AF_INET,SOCK_DGRAM)
+
+def receive_message():
+    while True:
+        try:
+            message, _ = clientSocket.recvfrom(2048)
+            print("\n" + message.decode())
+        except:
+            break
+
+
+while True:
+    passkey = input("Masukkan Passkey: ")
+    clientSocket.sendto(passkey.encode(),(serverName,serverPort))
+    Response, _ =clientSocket.recvfrom(2048)
+    if Response.decode() == 'OK':
+        print("Passkey benar, kamu sudah masuk chatroom")
+        break
+    else:
+        print(Response.decode())
+
+username = input("Masukkan nama anda: ")
+clientSocket.sendto(username.encode(), (serverName,serverPort))
+
+threading.Thread(target=receive_message, daemon=True).start()
+
+while True:
+    message = input("")
+    clientSocket.sendto(message.encode(),(serverName,serverPort))
+    pesan, serverAddress = clientSocket.recvfrom(2048)
+    message ="Permintaan acak"
+    clientSocket.sendto(message.encode(),(serverName,serverPort))
+    print("Bilangan Acak dari server("+str(serverAddress)+"): "+str(pesan.decode()))
