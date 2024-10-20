2ebcb92caec703f4d40146e95c4e49eccb6b7b68
Mark(ntrlnm) <166894547+DoItMark@users.noreply.github.com>
2024-10-20 06:35:24 +0000

    -m "applying caesar cipher"

--- src/UDPClient.py
+++ src/UDPClient.py
@@ -19,7 +19,24 @@ def encrypt(message): #+5
 
 def decrypt(message):#-5
     return encrypt(message, -5)  # Decrypt by shifting backwards
-    
+
+def validation():
+    while True:
+        try:
+            ip = str(input("Masukkan IP Tujuan: "))
+            inet_aton(ip)
+            port = int(input("Masukkan Port Tujuan: "))
+            if port < 1024 or port > 65535:
+                    raise ValueError("Port number must be between 1024 and 65535")
+            return ip, port
+        except OSError:
+            print("Invalid IP address format, please try again.")
+        except ValueError as ve:
+            print(f"Invalid input: {ve}")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
+
 def receive_message():
     while True:
         try:
@@ -30,24 +47,51 @@ def receive_message():
             break
 
 
-while True: #
-    passkey = input("Masukkan Passkey: ")
-    clientSocket.sendto(passkey.encode(),(serverName,serverPort))
-    Response, _ =clientSocket.recvfrom(2048)
-    if Response.decode() == 'OK':
-        print("Passkey benar, kamu sudah masuk chatroom")
-        break
-    else:
-        print(Response.decode())
 
-username = input("Masukkan nama anda: ")
-clientSocket.sendto(username.encode(), (serverName,serverPort))
+
+def login():
+    print("\n"*3)
+    print("========================LOGIN========================")
+    while True: #
+        passkey = input("Masukkan Passkey: ")
+        clientSocket.sendto(passkey.encode(),(serverName,serverPort))
+        Response, _ =clientSocket.recvfrom(2048)
+        if Response.decode() == 'OK':
+            print("Passkey benar, kamu sudah masuk chatroom")
+            username()
+            break
+        else:
+            print(Response.decode())
+def username():
+    print("\n"*3)
+    print("========================LOGIN========================")
+    while True:
+            username = input("Masukkan nama anda: ")
+            clientSocket.sendto(username.encode(), (serverName,serverPort))
+            receive_message, _ = clientSocket.recvfrom(2048)
+            if receive_message.decode() == "Berhasil":
+                print("Username accepted, joining the chatroom...")
+                break
+            else:
+                print(receive_message.decode())
+
+def handle():
+    print("\n"*3)
+    print("=======================CHATROOM=======================")
+    while True:
+        message = input("")
+        if message.lower() == 'exit':
+            clientSocket.sendto(message.encode(),(serverName,serverPort))
+            break
+        clientSocket.sendto(encrypt(message,5,True).encode(),(serverName,serverPort))
+
+serverName, serverPort = validation()
+clientSocket.connect((serverName, serverPort))
+print(f"Connected to server {serverName} on port {serverPort}")
+login()
 
 threading.Thread(target=receive_message, daemon=True).start()
-
-while True:
-    message = input("")
-    encMessage = encrypt(message)
-    clientSocket.sendto(encMessage.encode(),(serverName,serverPort))
-
+handle()
+print("=======================BYE BYE!=======================")
+clientSocket.close()
 
